39fce6f7c46547929fd1e10b3021bd196e45928a
jaewook <wodnr5050@gmail.com>
2024-05-09 14:16:15 +0900

    논리곱 논리합

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
 function App() {
-  return <></>;
+  const name = "강인";
+  const age = 19; // or 30;
+  return (
+    <div>
+      <p>{name}</p>
+      {name === "흥민" && <p>공격수</p>}
+      {name === "강인" && <p>미드필더</p>}
+
+      {age >= 20 && <p>투표 가능</p>}
+      {age < 20 && <p>투표 불가능</p>}
+    </div>
+  );
 }
 
 export default App;
