ea3e8ca6a71161603c21b7caf148a8736425f9da
jaewook <wodnr5050@gmail.com>
2024-05-09 15:29:02 +0900

    자바스크립트 객체(object)도 참조값을 저장한다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
 import React from "react";
 
 function App(props) {
+  const a = {
+    name: "John",
+  };
+
+  const b = a;
+
+  console.log(a === b);
+
+  console.log("a.name", a.name);
+  console.log("b.name", b.name);
+
+  b.name = "Jane";
+
+  console.log("a.name", a.name);
+  console.log("b.name", b.name);
+
   return <div></div>;
 }
 
