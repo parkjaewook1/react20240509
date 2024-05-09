16ac8bc3506e436fe87906e30512cb41500a73d4
jaewook <wodnr5050@gmail.com>
2024-05-09 14:32:25 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,4 @@
 function App() {
-  // &&, ||
-
-  // falsy(false 같은)
-  // : false, 0, "", null, undefined
-
-  const a = 0;
-
-  if (a) {
-    console.log("a는 참이다");
-  } else {
-    console.log("a는 거짓이다");
-  }
-
-  // truthy(true 같은) : falsy가 아니면 다 truthy
-
-  // &&, || 와 조합
-  let b = 0;
-
-  let c = b && "값이 있음";
-  console.log("c", c);
-
-  let d = b || "값이 없음";
-  console.log("d", d); // 값이 없음
-
-  return <div></div>;
+  return null;
 }
-
 export default App;
