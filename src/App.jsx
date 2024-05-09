91aaf6f31af2578a6b2c214b363551b520bab5c3
jaewook <wodnr5050@gmail.com>
2024-05-09 12:25:49 +0900

    코드 작성 연습 끝

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+function someMethod() {
+  return "hello js";
+}
+function someMethod2(param) {
+  return param + param;
+}
+
 function App() {
-  return <></>;
+  const a = 5;
+  const b = 9;
+  return (
+    <>
+      <div>
+        <p>
+          {a} X {b} = {a * b}
+        </p>
+        <p>{someMethod()}</p>
+        <p>{someMethod2("react")}</p>
+      </div>
+    </>
+  );
 }
 
 export default App;
