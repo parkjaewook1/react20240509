c0a08808e64175630796e2ee74beebd06c80b1cf
jaewook <wodnr5050@gmail.com>
2024-05-10 10:04:59 +0900

    onClick property와 함수 사용

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,40 @@
-rsf;
+import React from "react";
+
+function App(props) {
+  // 이름있는 함수
+  function action1() {
+    console.log("Action 1");
+  }
+  action1();
+
+  // 이름없는 함수
+  const action2 = function () {
+    console.log("anonymous action ");
+  };
+
+  // arrow function
+  const action3 = () => console.log("anonymous action $$$");
+
+  return (
+    <div>
+      <button onClick={action1}>클릭!</button> <br />
+      <button onClick={action2}>클릭@</button> <br />
+      <button
+        onClick={function () {
+          console.log("anonymous action ##### ");
+        }}
+      >
+        클릭#
+      </button>{" "}
+      <br />
+      <button onClick={action3}>클릭$</button> <br />
+      <button onClick={() => console.log("arrow function %%%")}>
+        클릭%
+      </button>{" "}
+      <br />
+      <br />
+    </div>
+  );
+}
+
+export default App;
