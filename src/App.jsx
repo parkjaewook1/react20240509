9c5715b26d32b99775e0302038134f1eb62ea29d
jaewook <wodnr5050@gmail.com>
2024-05-10 12:24:57 +0900

    얕은복사, 깊은복사 연습 완료

--- src/App.jsx
+++ src/App.jsx
@@ -10,22 +10,48 @@ function App(props) {
     },
   };
 
-  // 얕은 복사(shallow copy)
+  // 얕은 복사 (shallow copy)
   const { ...b } = a;
   b.age = 55;
-  console.log("b.age", b.age);
-  console.log("a.age", a.age);
+  console.log("b.age", b.age); // 55
+  console.log("a.age", a.age); // 44
 
-  console.log("a.address.city", a.address.city);
-  console.log("b.address.city", b.address.city);
+  b.address.city = "busan";
+  console.log("a.address.city", a.address.city); // busan
+  console.log("b.address.city", b.address.city); // busan
 
-  // 깊은 복사(deep copy)
+  // 깊은 복사 (deep copy)
   // 얕은 복사를 여러번 해서 해결
-  //
-  const { ...c } = a;
-  const { ...address1 } = a.address;
-  c.address = address1;
+  const { ...c } = a; // 얕은 복사
+  const { ...address1 } = a.address; // 얕은 복사
+  c.address = address1; // 깊은 복사
   c.address.city = "london";
+  console.log("a.address.city", a.address.city); // busan
+  console.log("c.address.city", c.address.city); // london
+
+  // 연습; 깊은 복사
+  const d = {
+    company: {
+      name: "apple",
+      location: "us",
+    },
+    name: "iphone",
+    price: 300,
+  };
+
+  // 코드 작성 : d 객체를 e 객체로 깊은 복사
+  const { ...e } = d;
+  const { ...company } = d.company;
+  e.company = company;
+
+  e.name = "galaxy";
+  e.company.name = "samsung";
+
+  console.log("d.name", d.name); // iphone
+  console.log("d.company.name", d.company.name); // apple
+  console.log("e.name", e.name); // galaxy
+  console.log("e.company.name", e.company.name); // samsung
+
   return <div></div>;
 }
 
