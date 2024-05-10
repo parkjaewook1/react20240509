f084550dc0f9f94c957b51132d487df03050a807
jaewook <wodnr5050@gmail.com>
2024-05-10 10:40:34 +0900

    구조분해할당 (destructuring assignment) 연습 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
 import React from "react";
 
 function App(props) {
+  const person = {
+    email: "test@example.com",
+    name: "test",
+    address: "gangnam",
+    country: "USA",
+  };
+
+  // todo: 이 위치에 적절한 코드 작성
+  const { email, name, address, country } = person;
+
+  console.log("email", email);
+  console.log("name", name);
+  console.log("address", address);
+  console.log("country", country);
+
   return <div></div>;
 }
 
