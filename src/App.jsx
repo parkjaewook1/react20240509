8010673631bf00202aa5ee2d1f87b59c32417ce2
jaewook <wodnr5050@gmail.com>
2024-05-10 12:31:34 +0900

    배열 복사를 분해할당의 나머지 모두로 해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,31 @@
 import React from "react";
 
 function App(props) {
+  // object ( {},[])
+  const a = [6, 7, 8];
+  const [x, y, z] = a;
+  console.log("x", x);
+  console.log("y", y);
+  console.log("z", z);
+
+  // 나머지 모두 (...변수면) rest
+  const [s, ...t] = a;
+  console.log("s", s);
+  console.log("t", t);
+  console.log("t[0]", t[0]);
+  console.log("t[1]", t[1]);
+
+  // 나머지 모두로 배열 복사 가능
+  const c = a; // 참조값 복사
+  const [...b] = a; // 각 원소를 새 배열에 복사
+
+  c[0] = 66;
+
+  console.log("a[0]", a[0]);
+  console.log("b[0]", b[0]);
+  console.log("c[0]", c[0]);
+
+  console.log("b", b);
   return <div></div>;
 }
 
