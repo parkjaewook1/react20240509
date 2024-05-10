02c0b33f70b28a9d562ee33696d510bee2376e2a
jaewook <wodnr5050@gmail.com>
2024-05-10 16:14:25 +0900

    style 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
+function MyComp(color) {
+  return (
+    <div style={{ color: color }}>
+      <h1>Lorem.</h1>
+      <p>Lorem ipsum dolor.</p>
+    </div>
+  );
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      {/*red color*/}
+      <MyComp />
+      {/*  blue color*/}
+      <MyComp />
+    </div>
+  );
 }
 
 export default App;
