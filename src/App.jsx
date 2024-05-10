773549adfa0c89ed089b0c03662078a96b5d4a55
jaewook <wodnr5050@gmail.com>
2024-05-10 14:38:33 +0900

    props를 분해할당으로 받기 연습 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
-function App(props) {
+function MyInfo({ name, team, salary, country }) {
+  console.log("name", name);
+  console.log("team", team);
+  console.log("salary", salary);
+  console.log("country", country);
   return <div></div>;
+}
+
+function App(props) {
+  return (
+    <div>
+      <MyInfo name="흥민" team="토트넘" salary={500} country="korea" />
+      <div>
+        <ul>
+          <li>이름 : 흥민</li>
+          <li>팀 : 토트넘</li>
+          <li>연봉 : 500</li>
+          <li>국가 : korea</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
