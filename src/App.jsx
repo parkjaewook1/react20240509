4622ab1032247714917bd26e3bb167ab8acf00e2
jaewook <wodnr5050@gmail.com>
2024-05-13 10:18:38 +0900

    import , export 연습 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { v1, v2 } from "./Values2.jsx";
+import { c1, v2 as c2 } from "./Values1.jsx";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <div>{v1}</div>
+      <div>{v2}</div>
+      <div>{c1}</div>
+      <div>{c2}</div>
+    </div>
+  );
 }
 
 export default App;
