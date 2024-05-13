e1b819d23152fde91b616f8aa32110c0c2553a8f
jaewook <wodnr5050@gmail.com>
2024-05-13 09:23:24 +0900

    children prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,32 @@
-rsf;
+import React from "react";
+
+function MyComp({ name, age, children }) {
+  console.log("name", name);
+  console.log("age", age);
+  console.log("children", children);
+
+  return (
+    <div>
+      <div>name : {name}</div>
+      <div>age : {age}</div>
+      <div>children : {children}</div>
+    </div>
+  );
+}
+
+function App(props) {
+  return (
+    <div>
+      <MyComp name={"son"} age={33}>
+        {/*content는 children prop*/}
+        <p>Lorem ipsum dolor.</p>
+      </MyComp>
+      <MyComp name={"lee"} age={22}>
+        {/*content는 children prop*/}
+        <p>Lorem ipsum dolor.</p>
+      </MyComp>
+    </div>
+  );
+}
+
+export default App;
