89ce293b15fed1682d972ffb7f907ab294086346
jaewook <wodnr5050@gmail.com>
2024-05-13 16:03:46 +0900

    input ,onChange 연습 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App(props) {
-  return <div></div>;
+  const [name, setName] = useState("");
+  const [hobby, setHobby] = useState("");
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          placeholder={"이름"}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          onChange={(e) => setHobby(e.target.value)}
+          placeholder={"취미"}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          onChange={(e) => setAddress(e.target.value)}
+          placeholder={"주소"}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          onChange={(e) => setCity(e.target.value)}
+          placeholder={"시티"}
+        />
+        <div>
+          {name},{hobby},{address},{city}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
