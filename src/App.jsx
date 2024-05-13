e02734df201e44cf54fa23a2809b755a7f13462a
jaewook <wodnr5050@gmail.com>
2024-05-13 14:11:36 +0900

    state 연습 시작 완료 1씩 증가/감소

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyNumber() {
+  const [number, setNumber] = useState(1000);
+  return (
+    <div>
+      {number} <hr />
+      <button onClick={() => setNumber(number + 1)}>+</button>
+      <button onClick={() => setNumber(number - 1)}>-</button>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyNumber />
+    </div>
+  );
 }
 
 export default App;
