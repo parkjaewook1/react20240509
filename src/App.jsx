f8481473e236faa7b2073bc3531a493dd6941dd5
jaewook <wodnr5050@gmail.com>
2024-05-13 14:13:07 +0900

    state 연습 시작 완료 1씩 증가/감소/곱/나눗

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 
 function MyNumber() {
-  const [number, setNumber] = useState(1000);
+  const [number, setNumber] = useState(5000000000);
   return (
     <div>
       {number} <hr />
       <button onClick={() => setNumber(number + 1)}>+</button>
       <button onClick={() => setNumber(number - 1)}>-</button>
+      <button onClick={() => setNumber(number * 10)}>X</button>
+      <button onClick={() => setNumber(number / 10)}>/</button>
     </div>
   );
 }
