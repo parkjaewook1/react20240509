e680146288433d28c27eeec0c1decf6e12e780b5
jaewook <wodnr5050@gmail.com>
2024-05-13 09:31:48 +0900

    코드리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
 import React from "react";
 
-function MyButton({ children }) {
-  return <button>{children}</button>;
-}
-
 function App(props) {
-  return (
-    <div>
-      <MyButton>클릭1</MyButton>
-      {/* <button>클릭1</button> */}
-      <MyButton>클릭2</MyButton>
-      {/* <button>클릭2</button> */}
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
