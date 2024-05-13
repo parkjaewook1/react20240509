0bd4eaa89fcff95fd9d4a8349412bf7af8bf47fe
jaewook <wodnr5050@gmail.com>
2024-05-13 16:36:46 +0900

    하나의 상태가 여러 property 를 갖는 경우 연습 완료

--- src/App.jsx
+++ src/App.jsx
@@ -17,49 +17,64 @@ function App(props) {
     setCar(nextCar);
   }
   function handleCompanyChange(e) {
-    const { ...nextCar } = car;
-    nextCar.company = e.target.value;
-    setCar(nextCar);
+    function handleChange(e) {
+      const { ...nextCar } = car;
+      nextCar.company = e.target.value;
+      nextCar[e.target.name] = e.target.value;
+      setCar(nextCar);
+    }
+    function handleModelChange(e) {
+      setCar({ ...car, model: e.target.value });
+    }
+
+    return (
+      <div>
+        <div>
+          <input type="text" placeholder={"이름"} onChange={handleNameChange} />
+          <input
+            type="text"
+            placeholder={"이름"}
+            name={"name"}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <input type="text" placeholder={"모델"} onChange={handleModelChange} />
+          <input
+            type="text"
+            placeholder={"모델"}
+            name={"model"}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder={"회사"}
+            onChange={handleCompanyChange}
+            name={"company"}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <input
+            type="number"
+            placeholder={"가격"}
+            onChange={handlePriceChange}
+            name={"price"}
+            onChange={handleChange}
+          />
+        </div>
+
+        <ul>
+          <li>이름 : {car.name}</li>
+          <li>모델 : {car.model}</li>
+          <li>회사 : {car.company}</li>
+          <li>가격 : {car.price}</li>
+        </ul>
+      </div>
+    );
   }
-  function handleModelChange(e) {
-    setCar({ ...car, model: e.target.value });
-  }
 
-  return (
-    <div>
-      <div>
-        <input type="text" />
-        <input type="text" placeholder={"이름"} onChange={handleNameChange} />
-      </div>
-      <div>
-        <input type="text" />
-        <input type="text" placeholder={"모델"} onChange={handleModelChange} />
-      </div>
-      <div>
-        <input type="text" />
-        <input
-          type="text"
-          placeholder={"회사"}
-          onChange={handleCompanyChange}
-        />
-      </div>
-      <div>
-        <input type="text" />
-        <input
-          type="number"
-          placeholder={"가격"}
-          onChange={handlePriceChange}
-        />
-      </div>
+  export default App;
 
-      <ul>
-        <li>이름 : {car.name}</li>
-        <li>모델 : {car.model}</li>
-        <li>회사 : {car.company}</li>
-        <li>가격 : {car.price}</li>
-      </ul>
-    </div>
-  );
-}
-
-export default App;
