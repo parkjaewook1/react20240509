c137c6e454f85b696a8c494a48554baea727b413
jaewook <wodnr5050@gmail.com>
2024-05-13 09:30:49 +0900

    children prop 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+function MyButton({ children }) {
+  return <button>{children}</button>;
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyButton>클릭1</MyButton>
+      {/* <button>클릭1</button> */}
+      <MyButton>클릭2</MyButton>
+      {/* <button>클릭2</button> */}
+    </div>
+  );
 }
 
 export default App;
