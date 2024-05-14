340a2cd5f8f08e6d4d6121765ffa2c566151224d
jaewook <wodnr5050@gmail.com>
2024-05-14 15:09:52 +0900

    코드리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 function App(props) {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    console.log("count 가 변경 될 때");
-  }, [count]);
-  useEffect(() => {
-    console.log("mount 될 때");
-  }, []);
-  return (
-    <div>
-      <button onClick={() => setCount(count + 1)}>click</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
