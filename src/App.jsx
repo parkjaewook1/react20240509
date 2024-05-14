b502b5e8d3da20d9356cc8d471b259b541463d2d
jaewook <wodnr5050@gmail.com>
2024-05-14 11:12:51 +0900

    lifting state up으로 여러 하위 컴포넌트에서 상태를 공유할 수 있음 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,39 @@
 import React, { useState } from "react";
 
-function MyButton({ count, onClick }) {
-  return (
-    <div>
-      <button onClick={onClick}>CLICK{count}</button>
-    </div>
-  );
-}
-
-function MyBox({ count }) {
-  return <div>{count} 번 클릭됨</div>;
-}
-
-function App(props) {
-  return <div></div>;
+function MyButton() {
   const [count, setCount] = useState(0);
 
-  function handleUpdateCount() {
-    setCount(count + 1);
+  function MyButton({ count, onClick }) {
+    return (
+      <div>
+        <button onClick={() => setCount(count + 1)}>CLICK {count}</button>
+        <button onClick={onClick}>CLICK {count}</button>
+      </div>
+    );
   }
 
-  return (
-    <div>
-      <MyButton count={count} onClick={handleUpdateCount} />
-      <hr />
-      <MyBox count={count} />
-    </div>
-  );
-}
+  function MyBox() {
+    return <div>{3} 번 클릭됨</div>;
+    function MyBox({ count }) {
+      return <div>{count} 번 클릭됨</div>;
+    }
+    function App(props) {
+      const [count, setCount] = useState(0);
 
-export default App;
+      function handleClick() {
+        setCount(count + 1);
+      }
+
+      return (
+        <div>
+          <MyButton />
+          <MyButton count={count} onClick={handleClick} />
+          <hr />
+          <MyBox />
+          <MyBox count={count} />
+        </div>
+      );
+    }
+
+    export default App;
+
