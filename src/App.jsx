3b36f9fd20d31c8d38a33385fa13237fa51a85c5
jaewook <wodnr5050@gmail.com>
2024-05-14 14:10:49 +0900

    컥테스트로 데이터 (객체) 전달 연습 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
-import React, { useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 
-function MyInput(onChange, text) {
+// step1
+const TextContext = createContext(null);
+
+function MyInput() {
+  // step 2
+  const textHandler = useContext(TextContext);
+
   return (
     <div>
-      <input type="text" onChange={(e) => onChange(e.target.value)} />
-      <p>{text}</p>
+      <input
+        type="text"
+        onChange={(e) => textHandler.updateText(e.target.value)}
+      />
+      <p>{textHandler.text}</p>
     </div>
   );
 }
 
-function MyText({ text }) {
+function MyText() {
+  const textHandler = useContext(TextContext);
   return (
     <div>
-      <p>{text}</p>
+      <p>{textHandler.text}</p>
     </div>
   );
 }
@@ -26,8 +36,11 @@ function App(props) {
 
   return (
     <div>
-      <MyInput text={text} onChange={handleUpdateText} />
-      <MyText text={text} />
+      {/*step3*/}
+      <TextContext.Provider value={{ text, updateText: handleUpdateText }}>
+        <MyInput />
+        <MyText />
+      </TextContext.Provider>
     </div>
   );
 }
