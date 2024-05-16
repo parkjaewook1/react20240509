19d3e80b9912019a020db22235b1f80a5156a1b3
jaewook <wodnr5050@gmail.com>
2024-05-16 11:24:38 +0900

    @RequestBody: json 파싱해서 map으로 받기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import axios from "axios";
 
+// json 은 post 방식
 function App(props) {
-  return <div></div>;
+  function handleClick1() {
+    const obj1 = { name: "son", age: 44 };
+    // axios 가 두번째 파라미터가 객체면 직렬화 해줌
+    axios.post("/api/main41/sub1", obj1);
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick1}>요청(json 데이터 포함)</button>
+    </div>
+  );
 }
 
 export default App;
