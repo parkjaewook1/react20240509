51bb4f77d27eeb7c58961419a9a72d6e97247817
jaewook <wodnr5050@gmail.com>
2024-05-16 14:19:39 +0900

    Spring 응답받기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  function handleClick1() {
+    axios.get("/api/main42/sub1").then((param) => console.log(param.data));
+  }
+
+  function handleClick2() {
+    axios.get("/api/main42/sub2").then((param) => console.log(param.data));
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick1}>응답받기</button>
+      <br />
+      <button onClick={handleClick2}>응답 받기</button>
+    </div>
+  );
 }
 
 export default App;
