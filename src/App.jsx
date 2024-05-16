312ed5554ed52ae3dfdb35e167f13bc8b1fc1a0f
jaewook <wodnr5050@gmail.com>
2024-05-16 09:36:46 +0900

    /api/someurl 의 get, post 요청핸들러메소드 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  function handleClickButton1() {
+    // ajax get 요청
+    axios.get("/api");
+  }
+
+  function handleClickButton2() {
+    // ajax post 요청
+    axios.post("/api/someurl");
+  }
+
+  return (
+    <div>
+      <button onClick={handleClickButton1}>get 요청</button>
+      <button onClick={handleClickButton2}>post 요청</button>
+      <button>post 요청</button>
+    </div>
+  );
 }
 
 export default App;
