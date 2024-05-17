69eca6019655e7f0d0059cefd9c2a65a692d08dd
jaewook <wodnr5050@gmail.com>
2024-05-17 16:08:11 +0900

    게시물 홈/ 게시물 목록

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,59 @@
 import React from "react";
+import {
+  createBrowserRouter,
+  Link,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+
+function Root() {
+  return (
+    <div>
+      <div>
+        <Link to="/">HOME</Link>
+        <Link to="/list">LIST</Link>
+      </div>
+      <hr />
+      <div>
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
+function BoardList() {
+  return (
+    <div>
+      <div>1번 게시물 보기</div>
+      <div>2번 게시물 보기</div>
+      <div>3번 게시물 보기</div>
+    </div>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <div>MAIN PAGE</div>,
+      },
+      {
+        path: "board",
+        element: <div> Board PAGE</div>,
+      },
+      {
+        path: "list",
+        element: <BoardList />,
+      },
+    ],
+  },
+]);
 
 function App(props) {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
