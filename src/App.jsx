c920f1a67d070bfd61592f2de9d36dc20c978301
jaewook <wodnr5050@gmail.com>
2024-05-17 15:14:15 +0900

    상위 경로 컴포넌트 추출, 스타일 줌

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 
+function SpringRoot() {
+  return (
+    <div>
+      <div
+        style={{
+          padding: "10px",
+          backgroundColor: "#eee",
+        }}
+      >
+        NAVER
+      </div>
+      <div style={{ marginTop: "20px" }}>
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
 const router = createBrowserRouter([
   {
     path: "spring",
-    element: (
-      <div>
-        spring root page <Outlet />
-      </div>
-    ),
+    element: <SpringRoot />,
     children: [
       { path: "api", element: <div>api page</div> },
       { path: "odc", element: <div>doc page</div> },
