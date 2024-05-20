2a78a17e4574c53600f949dda8c8db551eb9880b
jaewook <wodnr5050@gmail.com>
2024-05-20 14:28:43 +0900

    이벤트 버블릭 막기 연습 stopPropagation 활용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { Badge, Box, Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  function handleButtonClick(e) {
+    e.stopPropagation();
+    console.log("버튼 클릭");
+  }
+
+  function handleBadgeClick(e) {
+    e.stopPropagation();
+    console.log("뱃지 클릭됨");
+  }
+
+  function handleBoxClick(e) {
+    e.stopPropagation();
+    console.log("박스 클릭됨");
+  }
+
+  return (
+    <ChakraProvider>
+      <Box onClick={handleBoxClick} w={"200px"} bgColor={"gray"} p={10}>
+        <Button onClick={handleButtonClick} colorScheme={"blue"}>
+          클릭
+          <Badge onClick={handleBadgeClick} ml={3}>
+            new
+          </Badge>
+        </Button>
+      </Box>
+    </ChakraProvider>
+  );
 }
 
 export default App;
