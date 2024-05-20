eff54564c36ee3f8d0556df3cca7c229eeb1a714
jaewook <wodnr5050@gmail.com>
2024-05-20 16:33:07 +0900

    배열 map 사용시 key prop 적용 할 것

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  const names = ["January", "February", "March", "April"];
+  // 배열을 map 메소드 사용해서 컴포넌트 만들 때
+  // key prop을 사용해서 효율성을 높일 것
+  // key prop에 값을 주로 primary key가 사용됨
+  return (
+    <div>
+      <ul>
+        {names.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+        ;
+      </ul>
+    </div>
+  );
 }
 
 export default App;
