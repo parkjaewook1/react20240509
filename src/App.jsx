1b7a9ad142e7e29aacb46316235cb30bfa05217a
jaewook <wodnr5050@gmail.com>
2024-05-20 14:05:23 +0900

    코드초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { ChakraProvider, useDisclosure, useToast } from "@chakra-ui/react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faApple } from "@fortawesome/free-brands-svg-icons";
+import React from "react";
 
 function App(props) {
-  const [count, setCount] = useState(0);
-  useEffect(() => {}, []);
-  const toast = useToast();
-  const { isOpen, onClose, onOpen } = useDisclosure();
-
-  // use... : hook
-  // if / for 내에서 사용하지 말 것
-
-  // 안전하게 hook은 컴포넌트 가장 상단에 순서대로 작성할 것
-  if (true) {
-    const [text, setText] = useState("");
-  }
-
-  <FontAwesomeIcon icon={faApple} />;
-
-  return (
-    <ChakraProvider>
-      <div></div>
-    </ChakraProvider>
-  );
+  return <div></div>;
 }
 
 export default App;
