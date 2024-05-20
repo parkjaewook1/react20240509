3174c37feb396e6c115b9be60eafe4a34b9e0142
jaewook <wodnr5050@gmail.com>
2024-05-20 15:05:32 +0900

    기본 동작 막기 preventDefault 연습 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  // a클릭시
+  // form 서브밋 시
+  function handleLinkClick(e) {
+    e.stopPropagation();
+    console.log("막힘");
+  }
+  function handleSubmitForm(e) {
+    e.preventDefault();
+    console.log("다른 동작");
+  }
+
+  // 기본 동작 막고,, 콘솔에 간단한 메세지 출력
+  return (
+    <div>
+      <a href="https://www.google.com" onClick={handleLinkClick}>
+        구글
+      </a>
+      <hr />
+      <form action="https://www.google.com" onSubmit={handleSubmitForm}>
+        <input type="text" />
+        <button>전송</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
