78ab18e01f26b7d8d8c931e1b5c139afc1ea6963
jaewook <wodnr5050@gmail.com>
2024-05-20 11:10:25 +0900

    form 컴포넌트들

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import {
   Box,
   Center,
   ChakraProvider,
+  Checkbox,
   FormControl,
   FormLabel,
   Input,
+  Radio,
+  RadioGroup,
+  Switch,
+  Textarea,
 } from "@chakra-ui/react";
 
 function App(props) {
@@ -14,12 +19,6 @@ function App(props) {
       <label htmlFor={"input1"}>이름 </label>
       <input id={"input1"} />
       <hr />
-      <Box w={{ base: "100%", md: "50%" }}>
-        <FormControl>
-          <FormLabel>이름</FormLabel>
-          <Input />
-        </FormControl>
-      </Box>
       <Center>
         <Box w={{ base: "100%", md: "50%" }}>
           <FormControl>
@@ -28,6 +27,21 @@ function App(props) {
           </FormControl>
         </Box>
       </Center>
+      <hr />
+      <Checkbox>lorem1</Checkbox>
+      <Checkbox>Lorem.</Checkbox>
+      <Checkbox>Quas.</Checkbox>
+      <Checkbox>Tempora.</Checkbox>
+      <hr />
+      <RadioGroup>
+        <Radio value="1">1</Radio>
+        <Radio value="2">2</Radio>
+        <Radio value="3">3</Radio>
+      </RadioGroup>
+      <hr />
+      <Switch />
+      <hr />
+      <Textarea></Textarea>
     </ChakraProvider>
   );
 }
